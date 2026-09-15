f423432bb93058fd50f28c6575210100d987e811
spaghettttto <a395016@uach.mx>
2026-09-15 08:51:43 -0700

    Refactor temperature input formatting in app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 st.title("Clasificador de temperatura")
 
-temperatura = st.number_input
-    ("Introduce la temperatura en °C:",
-    value=20
-)
+temperatura =st.number_input("Introduce la temperatura en °C:",
+    value=20)
 
 #Escribe aquí la estructura if-elif-else
 if temperatura <10:
