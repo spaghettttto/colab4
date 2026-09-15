0d5b71724f91a5466126859bee8b771af22fcbff
spaghettttto <a395016@uach.mx>
2026-09-15 08:37:06 -0700

    Correct temperature conditional statements
    Fix syntax errors in temperature conditionals.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ colab4
 
 if temperatura <10:
   st.write ("Hace frío")
-elif temperatura <=24
+elif temperatura <=24:
   st.write ("La temperatura es agradable")
-else temperatura
+else: temperatura 
   st.write ("Hace calor")
