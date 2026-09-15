31105899ae575a225e1eb5ba009f16a8829f7298
spaghettttto <a395016@uach.mx>
2026-09-15 08:33:15 -0700

    Fix syntax errors in temperature classifier

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # Clasificador de temperatura
 colab4
 
-if temperatura <10:
+if temperatura <10
   st.write ("Hace frío")
-elif temperatura <=24:
+elif temperatura <=24
   st.write ("La temperatura es agradable")
-else temperatura:
+else temperatura
   st.write ("Hace calor")
