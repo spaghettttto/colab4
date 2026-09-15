59e988525d1f62a42e3d1d9a45d74b2f8039e797
spaghettttto <a395016@uach.mx>
2026-09-15 08:49:04 -0700

    Implement temperature classification logic
    Added temperature input and conditional statements for classification.

--- .py
+++ .py
@@ -4,8 +4,10 @@ st.title("Clasificador de temperatura")
 
 temperatura = st.number_input
     ("Introduce la temperatura en °C:",
-    value=20)
+    value=20
+)
 
+#Escribe aquí la estructura if-elif-else
 if temperatura <10:
 st.write("Hace frío")
 elif temperatura <=24:
