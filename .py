66de319a81a4ddaba342d7c2a3b73c013e623254
spaghettttto <a395016@uach.mx>
2026-09-15 08:45:42 -0700

    Refactor temperature classifier with Streamlit

--- .py
+++ .py
@@ -1,9 +1,15 @@
-# Clasificador de temperatura
-colab4
+
+import streamlit as st
+
+st.title("Clasificador de temperatura")
+
+temperatura = st.number_input
+    ("Introduce la temperatura en °C:",
+    value=20)
 
 if temperatura <10:
-    st.write ("Hace frío.")
+   st.write("Hace frío")
 elif temperatura <=24:
-    st.write ("La temperatura es agradable.")
-else:
-    st.write("Hace calor.")
+   st.write("La temperatura es agradable")
+else temperatura:
+  st.write("Hace calor")
