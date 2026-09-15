c9bedcb245a4be8e98481bb104ecb280b8b5a911
spaghettttto <a395016@uach.mx>
2026-09-15 08:39:42 -0700

    Update temperature messages for clarity
    Added periods to temperature classification messages.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 colab4
 
 if temperatura <10:
-  st.write ("Hace frío")
+     st.write ("Hace frío.")
 elif temperatura <=24:
-  st.write ("La temperatura es agradable")
+     st.write ("La temperatura es agradable.")
 else: temperatura 
-  st.write ("Hace calor")
+     st.write("Hace calor.")
